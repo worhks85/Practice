cadd58440a51fae6bfc0d8ef3d338830eb2a8ec2
JaeKwan Kim <worhks5017@naver.com>
2022-02-28 23:14:46 +0900

    2/28 Practice

---
+++ src/OperatorEx.java
@@ -0,0 +1,26 @@
+
+public class OperatorEx {
+	public static void main(String[] args) {
+		int i=5, j=0;
+
+		// j=i;
+		// ++i;
+		
+		j = i++;
+		System.out.println("j=i++ 실행 후, i=" + i +", j="+ j);
+
+		i=5;        
+		j=0;
+		
+		
+		//++i;
+		//j=i
+
+		j = ++i;
+		System.out.println("j=++i 실행 후, i=" + i +", j="+ j);
+		
+		
+
+	}
+
+}
