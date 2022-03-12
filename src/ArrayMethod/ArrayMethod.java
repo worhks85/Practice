043dfa6efb340967ca66be221feddd487155fc69
qudrh <qudrh@DESKTOP-9J5DH18>
2022-03-12 14:31:17 +0900

    3/12 Practice

---
+++ src/ArrayMethod/ArrayMethod.java
@@ -0,0 +1,21 @@
+package ArrayMethod;
+
+public class ArrayMethod {
+
+	public static void main(String[] args) {
+		String names[] = {"Steven Paul Jobs",
+		                 "Bill Gates"
+		                 ,"Mark Elliot Zuckerberg",
+		                 "Elon Musk",
+		                 "Jeff Bezos",
+		                 "Warren Edward Buffett",
+		                 "Larry Page",
+		                 "Larry Ellison",
+		                 "Tim Cook",
+		                 "Lioyd Blankfein"};
+		for (int i = 0 ; i < names.length; i++) System.out.println(names[i]);
+		
+		
+	}
+
+}
--- src/BackJun/String/Quiz9935.java
+++ src/BackJun/String/Quiz9935.java
@@ -1,14 +1,15 @@
 package BackJun.String;
 
 
-public class  input() {
-	
-}
 public class Quiz9935 {
 
 	public static void main(String[] args) {
-
-
+		
+		
+		
+		
+		
+		
 	}
 
 }
