a53833167c72584730ed8580752af5edf747622b
JaeKwan Kim <worhks5017@naver.com>
2022-03-19 23:31:15 +0900

    3/19 Practice

---
+++ src/Thread/ThreadEx1.java
@@ -0,0 +1,37 @@
+package Thread;
+
+/*
+ * 프로세스 : 실행중인 프로그램
+ * 멀티테스킹 : 동시에 여러가지 작업을 실행
+ * 멀티스레드 : 두개 이상의 메소드가 동시에 실행하는것
+ * 
+ * 
+ * 1. Thread클래스를 상속받음
+ * 2. public void run() 메소드를 오버라이드 함
+ * 3. start() 메소드를 호출하여 실행시킴
+ * 
+ * runnable 인터페이스를 이용하는 방법
+ * 1. runnable 인터페이스를 상속받음
+ * 2. public void run() 메소드를 오버라이드 함
+ * 3. 생성자에 runnalbe 상속받은 클래스의 객체를 넣어 생성
+ * 
+ *  
+ *  static int activeCount() - 현재 실행중인 thread의 갯수를도렬줌
+ *  static Thread currentThread()  - 현재 실행중의 thread 객체의 참조를 돌려줌
+ *  
+ *  
+ *  
+ *  
+ */
+
+
+public class ThreadEx1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
