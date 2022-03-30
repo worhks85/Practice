c93014401d03f2bdfe431031778ef32a74c8e52e
JaeKwan Kim <worhks5017@naver.com>
2022-03-30 21:29:09 +0900

    3/30 Practice

---
+++ src/byteIO/Ex01.java
@@ -0,0 +1,34 @@
+package byteIO;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+ * byte 출력 
+ * - 스트림은 바이트 단위로 데이터를 전송
+ * - FileOutputSteam 클래스를 이용하여 출력
+ * 
+ * 주로 write()
+ * 
+ * FileInputStream클래스를 이용하여 입력
+ * - read() 메소드를 이용하여 파일의 자료를 가져옴
+ * - 데이터의 끝은 -1
+ * 
+ * 
+ */
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException{
+		File dir = new File("C:"+File.separator+"Users"+File.separator+"worhk"+File.separator+"Desktop"+File.separator+"Zavaworkspace"+File.separator+"_ch02");
+		File f = new File(dir,"aaa.txt");
+		
+		FileOutputStream fos = new FileOutputStream(f,true);
+		//객체 생성시 true이면 이어쓰기 false이면 다시쓰기
+		//fos.write(65);
+		fos.write('B');
+		
+		
+	}
+
+}
