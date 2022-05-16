0bb4342f9536323ddf504fb009cb4b2b00bf5b79
JaeKwan Kim <worhks5017@naver.com>
2022-05-16 22:57:29 +0900

    5/16 Practice

---
+++ src/BackJun/Quiz2562.java
@@ -0,0 +1,17 @@
+package BackJun;
+import java.io.*;
+import java.util.*;
+public class Quiz2562 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> arr =new ArrayList<Integer>();
+		while (true) {
+			int n = Integer.parseInt(br.readLine());
+			arr.add(n);
+			
+		}
+		
+	}
+
+}
